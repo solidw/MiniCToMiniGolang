2c1fa953a3472f8fd1a0a7a23cc2d0893e15ecc6
solidw <solidw@naver.com>
2019-11-26 19:57:11 +0900

    Upload base compiler which is MiniCToBytecode Compiler

---
+++ test.c
@@ -0,0 +1,18 @@
+int add(int a, int b) {
+	int c;
+	c = a+b;
+	return c;
+}
+
+void main() {
+	int d;
+	d = 5;
+
+	while(d != 1) {
+		if ( d == 3 or d == 2 ) {
+		_print(d);
+
+		}
+		d = d - 1;
+	}
+}
